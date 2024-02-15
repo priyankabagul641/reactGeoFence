34cce67cd636b442f5fb29fe1412086c50cc5f7e
priyanka_bagul <priyanka.bagul@tablabs.in>
2024-02-15 21:29:19 +0530

    updateWork

--- App.js
+++ App.js
@@ -1,12 +1,74 @@
-import { StatusBar } from 'expo-status-bar';
+// import React, { useState, useEffect } from 'react';
+// import { Platform, Text, View, StyleSheet } from 'react-native';
+// import * as Device from 'expo-device';
+// import * as Location from 'expo-location';
+
+// export default function App() {
+//   const [location, setLocation] = useState(null);
+//   const [errorMsg, setErrorMsg] = useState(null);
+
+//   useEffect(() => {
+//     (async () => {
+//       if (Platform.OS === 'android' && !Device.isDevice) {
+//         setErrorMsg(
+//           'Oops, this will not work on Snack in an Android Emulator. Try it on your device!'
+//         );
+//         return;
+//       }
+//       let { status } = await Location.requestForegroundPermissionsAsync();
+//       if (status !== 'granted') {
+//         setErrorMsg('Permission to access location was denied');
+//         return;
+//       }
+
+//       let location = await Location.getCurrentPositionAsync({});
+//       setLocation(location);
+//     })();
+//   }, []);
+
+//   let text = 'Waiting..';
+//   if (errorMsg) {
+//     text = errorMsg;
+//   } else if (location) {
+//     text = JSON.stringify(location);
+//   }
+
+//   return (
+//     <View style={styles.container}>
+//       <Text style={styles.paragraph}>{text}</Text>
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//     padding: 20,
+//   },
+//   paragraph: {
+//     fontSize: 18,
+//     textAlign: 'center',
+//   },
+// });
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import { StyleSheet, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView style={styles.map} 
+        provider={PROVIDER_GOOGLE}
+        apiKey={AIzaSyAm332fBuy8QoCC6ZFv7pizIqdmaT-jz30}
+       initialRegion={{
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      }}
+      />
     </View>
   );
 }
@@ -14,8 +76,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  map: {
+    width: '100%',
+    height: '100%',
   },
 });
